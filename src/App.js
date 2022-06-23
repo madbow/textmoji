b58afc6631ce429d0cc1b3f5cde889aa18349b77
abdi <abdirhm9@gmail.com>
2022-06-23 16:47:42 +0700

    release: textmoji

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const alpUpper = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) )
 
@@ -6,73 +6,161 @@ const alpLower = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode(
 
 const char = [...alpUpper, ...alpLower]
 
-const emoUpper = Array.from("😀🤣😌😛😕😢🥵🤗🤥😯😪🤧😷😮😦😶🤔🥶😭🙁🤩😝😍🤤😃😷🤕")
+const emoUpper = Array.from("🤣😌😛😕😢🥵🤗🤥😯😪🤧😷😮😦😶🤔🥶😭🙁🤩😝😍🤤😃😷🤕")
 
-const emoLower = Array.from("😄🥰😜🥳😤😠😣😏🤪😘😊😁😆😇😗🤨😒😖😡😨😲🤐🤑🤢🤮😴🤤")
+const emoLower = Array.from("🥰😜🥳😤😠😣😏🤪😘😊😁😆😇😗🤨😒😖😡😨😲🤐🤑🤢🤮😴🤤")
 
-const emoSpace = Array.from("😵🤬🤫")
+const emoSpace = Array.from("😵🤬🤫😀")
   
 const emoji = [...emoUpper, ...emoLower]
   
 const TYPES = Object.freeze({
   transform: {
-    toEmoji: 'emoji',
-    toChar: 'char'
+    emoji: 'emoji',
+    char: 'char'
   }
 })
 
 const App = () => {
   const [textInput, setTextInput] = useState('')
-  const [passkey, setPasskey] = useState(0)
-  const [output, setOutput] = useState('')
+  const [passkey, setPasskey] = useState('')
+  const [emojiInput, setEmojiInput] = useState('')
+  const [activeTextArea, setActiveTextArea] = useState(TYPES.transform.char)
 		
   const transform = (type) => { 
-    if (type === TYPES.transform.toEmoji) generateTextmoji()
-    else if (type === TYPES.transform.toChar) generateText()
+    if (type === TYPES.transform.emoji) generateText()
+    else if (type === TYPES.transform.char) generateTextmoji()
   }
 
+
+
   const generateTextmoji = () => {
-    const key = passkey % (26 * 2)
+    let key = (passkey % (26 * 2)) ?? 0
     const text = textInput.split('')
+    const charArray = char.slice()
+    const newChar = [...charArray.splice(key), ...charArray.splice(0, key)]
+    const emojiArray = emoji.slice()
+    const newEmoji = [...emojiArray.splice(key), ...emojiArray.splice(0, key)]
     let arr = []
     text.forEach(e => {
       const randomSpace = emoSpace[Math.floor(Math.random() * (emoSpace.length - 1))]
       let i
-      i = char.findIndex(ce => ce === e)
+      i = newChar.findIndex(ce => ce === e)
       if (e === ' ') i = randomSpace
       else if (i < 0) i = e
-      else i = emoji[i]
+      else i = newEmoji[i]
       arr.push(i)
     })
-
-    setOutput(arr)
+    setEmojiInput(arr.join('')) 
   }
 
-  const generateText = async () => {
-    const key = passkey % (26 * 2)
-    const text = [...textInput]
+  const generateText = () => {
+    let key = (passkey % (26 * 2)) ?? 0
+    const text = [...emojiInput]
+    const emojiArray = emoji.slice()
+    const newEmoji = [...emojiArray.splice(key), ...emojiArray.splice(0, key)]
+    const charArray = char.slice()
+    const newChar = [...charArray.splice(key), ...charArray.splice(0, key)]
+
     let arr = []
     text.forEach(e => {
       let i
-      i = emoji.findIndex(ce => ce === e)
+      i = newEmoji.findIndex(ce => ce === e)
       if (emoSpace.includes(e)) i = ' '
       else if (i < 0) i = e
-      else i = char[i]
+      else i = newChar[i]
       arr.push(i)
     })
-
-    setOutput(arr)
+    setTextInput(arr.join(''))
   }
-console.log(Math.floor(Math.random() * 2))
+
+  const changeActiveArea = () => {
+    // setEmojiInput('')
+    // setTextInput('')
+    activeTextArea === TYPES.transform.char ? setActiveTextArea(TYPES.transform.emoji) : setActiveTextArea(TYPES.transform.char)
+  }
+
+  const onChangePasskey = (e) => {
+    let key = e.target.value
+
+    if (!isNaN(key)) {
+      return setPasskey(key)
+    }
+
+    alert('Passkey should be a number')
+    return setPasskey('')
+  }
+
+  useEffect(() => {
+    transform(activeTextArea)
+  }, [textInput, emojiInput, activeTextArea, passkey])
+  
+
   return (
-    <div>
-      <div>text input: {textInput}</div>
-      <input onChange={e => setTextInput(e.target.value)}/>
-      <button onClick={() => transform(TYPES.transform.toEmoji)}>to Emoji</button>
-      <button onClick={() => transform(TYPES.transform.toChar)}>to Char</button>
-      <div>output: {output}</div>
-      <div>math random: {}</div>
+    <div className="flex justify-center pt-20">
+    <div className="mb-3 xl:w-96 ">
+      <label for="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-gray-700">Text</label>
+      <textarea
+        onChange={e => setTextInput(e.target.value)}
+        value={textInput}
+        disabled={!!(activeTextArea !== TYPES.transform.char)}
+        className={`
+          ${activeTextArea !== TYPES.transform.char ? 'bg-gray-200 ' : ''}
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        `}
+        id="exampleFormControlTextarea1"
+        rows="3"
+        placeholder="Your message"
+      ></textarea>
     </div>
+    <div className='w-40 flex justify-center flex-col items-center ml-10'>
+      <button className='mb-5 border border-gray-200 rounded-lg bg-white h-10 w-10 self-center text-gray-700' onClick={changeActiveArea}>{activeTextArea === TYPES.transform.emoji ? '<' : '>'}</button>
+      <input value={passkey} inputMode='numeric' onChange={onChangePasskey} className='w-40 p-2 border text-gray-700' placeholder='Passcode' />
+    </div>
+    <div className="mb-3 xl:w-96 ml-10">
+      <label for="exampleFormControlTextarea1" className="form-label inline-block mb-2 text-gray-700">Emoji</label>
+      <textarea
+        onChange={e => setEmojiInput(e.target.value)}
+        disabled={!!(activeTextArea !== TYPES.transform.emoji)}
+        value={emojiInput}
+        className={`
+        ${activeTextArea !== TYPES.transform.emoji ? 'bg-gray-200 ' : ''}
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-500 focus:outline-none
+        `}
+        id="exampleFormControlTextarea1"
+        rows="3"
+        placeholder="Your emoji message"
+      ></textarea>
+    </div>
+  </div>
   )
 }
 
